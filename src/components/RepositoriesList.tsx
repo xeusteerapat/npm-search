8cf7cde52346821ea2e89c529fc3a73bcf7988e3
xeusteerapat <xeus085@gmail.com>
2021-01-16 22:48:17 +0700

    [EDIT] wire up state to component

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setTerm('');
+  };
+
   return (
     <div>
-      <form>
-        <input type='text' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
