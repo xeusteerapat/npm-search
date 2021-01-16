95165cddae1e2fc185d9d6cf822cd6b70bc87ef7
xeusteerapat <xeus085@gmail.com>
2021-01-16 20:24:32 +0700

    [EDIT] update action type with enum and type guard

--- src/state/reducers/repositoriesReducers.ts
+++ src/state/reducers/repositoriesReducers.ts
@@ -4,23 +4,49 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+type Action =
+  | SearchRepositoriesAction // type union
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'SEARCH_REPOSITORIES',
+  SEARCH_REPOSITORIES_SUCCESS = 'SEARCH_REPOSITORIES_SUCCESS',
+  SEARCH_REPOSITORIES_ERROR = 'SEARCH_REPOSITORIES_ERROR',
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
   switch (action.type) {
-    case 'SEARCH_REPOSITORIES':
+    case ActionType.SEARCH_REPOSITORIES:
       return {
         loading: true,
         error: null,
         data: [],
       };
 
-    case 'SEARCH_REPOSITORIES_SUCCESS':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
       return {
         loading: false,
         error: null,
         data: action.payload,
       };
 
-    case 'SEARCH_REPOSITORIES_ERROR':
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
       return {
         loading: false,
         error: action.payload,
